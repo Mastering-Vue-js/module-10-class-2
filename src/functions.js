1606ecd9f7a06befee2e7f4dca83dfc21b059778
Sayedul Sayem <sayedulsayem@gmail.com>
2024-02-29 21:19:16 +0600

    Optimize functions

--- src/functions.js
+++ src/functions.js
@@ -1,32 +1,23 @@
 const baseUrl = "https://jsonplaceholder.typicode.com";
 const fn = {
     async fetchPublicApi(endPoint = "", params = "", requestType = "GET") {
-        if (requestType.toUpperCase() == "POST" || "PUT" == requestType.toUpperCase()) {
-            const res = await fetch(baseUrl + endPoint, {
-                method: requestType.toUpperCase(),
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    Accept: "application/vnd.api+json",
-                    "Content-Type": "application/vnd.api+json",
-                },
-                body: JSON.stringify(params),
-            });
-
-            const response = await res.json();
-            return response;
-        } else {
-            const res = await fetch(baseUrl + endPoint, {
-                method: requestType.toUpperCase(),
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    Accept: "application/vnd.api+json",
-                    "Content-Type": "application/vnd.api+json",
-                },
-            });
-
-            const response = await res.json();
-            return response;
+        let request = {
+            method: requestType.toUpperCase(),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                Accept: "application/vnd.api+json",
+                "Content-Type": "application/vnd.api+json",
+            },
         }
+
+        if (requestType.toUpperCase() == "POST" || "PUT" == requestType.toUpperCase()) {
+            request.body = JSON.stringify(params);
+        }
+
+        const res = await fetch(baseUrl + endPoint, request);
+
+        const response = await res.json();
+        return response;
     },
 };
 
